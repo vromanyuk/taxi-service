3fb462d9d4142650a0699c98cc0b26c6717b124f
Vitalii Romanyuk <vitaliy37777@gmail.com>
2023-04-18 15:35:52 +0300

    load project on GitHub

---
+++ src/main/java/taxi/service/DriverServiceImpl.java
@@ -0,0 +1,49 @@
+package taxi.service;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+import taxi.dao.DriverDao;
+import taxi.exception.AuthenticationException;
+import taxi.lib.Inject;
+import taxi.lib.Service;
+import taxi.model.Driver;
+
+@Service
+public class DriverServiceImpl implements DriverService {
+    @Inject
+    private DriverDao driverDao;
+
+    @Override
+    public Driver create(Driver driver) {
+        return driverDao.create(driver);
+    }
+
+    @Override
+    public Driver get(Long id) {
+        return driverDao.get(id).orElseThrow(() ->
+            new NoSuchElementException("Can't get driver by id: " + id)
+        );
+    }
+
+    @Override
+    public List<Driver> getAll() {
+        return driverDao.getAll();
+    }
+
+    @Override
+    public Driver update(Driver driver) {
+        return driverDao.update(driver);
+    }
+
+    @Override
+    public boolean delete(Long id) {
+        return driverDao.delete(id);
+    }
+
+    @Override
+    public Driver findByLogin(String login) throws AuthenticationException {
+        return driverDao.findByLogin(login).orElseThrow(() ->
+                new AuthenticationException("Can't get driver by login: " + login)
+        );
+    }
+}
